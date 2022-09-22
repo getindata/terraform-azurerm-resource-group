461068e67eadb84c0124fef925f78e5fe5fc2c49
Jakub Igla <jakub.igla@getindata.com>
2022-09-22 13:08:40 +0200

    feat: Use formatdate function for default value for start_date option

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   consumption_budget_defaults = {
     time_grain = "Monthly"
     consumption_budget_start_date = (length(time_static.consumption_budget_start_date) == 0 ? null :
-    format("%s-01T00:00:00Z", substr(one(time_static.consumption_budget_start_date[*].id), 0, 7)))
+    formatdate("YYYY-MM-01'T'00:00:00Z", one(time_static.consumption_budget_start_date[*].rfc3339)))
     notifications = {
       contact_emails = var.default_consumption_budget_notification_emails
       operator       = "EqualTo"
